0ce1580d3ccd14d9e0408761c527ffc89c135ac1
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-08-24 16:31:27 +0530

    Learning UseEffect hook how it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [name, setName] = useState("");
+
+  // On every Render
+  useEffect(() => {
+    // window.addEventListener("resize", updateWindowWidth);
+    console.log("I re-rendered");
+  });
+
+  // On first Render/ Mount only! - componentDidMount Alternative
+  useEffect(() => {
+    console.log("The Component Mounted");
+  }, []);
+
+  // On first Render + whenever dependency changes! - componentDidUpdate Alternative
+  useEffect(() => {
+    // console.log(`The name changed: ${name}`);
+  }, [name]);
+
+  // Follows the same rules, except this handles the unmounting on a component! - componentWillUnmount Alternative
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowWidth);
+
+    return () => {
+      // when component unmounts, thiscleanup code runs....
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
+
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1>The UseEffect Hook</h1>
+        <h2>The Window Width is: {windowWidth}</h2>
+
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter a name"
+        />
+      </center>
     </div>
   );
-}
+};
 
 export default App;
